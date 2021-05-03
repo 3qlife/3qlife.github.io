556e3de9b8ec5226213515f1cd141389c25bf8f2
dajin.gao@gmail.com <dajin.gao@gmail.com>
2021-05-03 12:17:42 +0900

    version 0.2

--- javascript/video.js
+++ javascript/video.js
@@ -1,10 +1,24 @@
+var playVideo = false;
+var videoUrl = "https://www.youtube.com/embed/_Xbubk0c9uQ?autoplay=1&playsinline=1";
+
+function changeVideoDisplay() {
+  if(playVideo) {
+    closeIFrame();
+  } else {
+    displayIFrame();
+  }
+}
+
 function displayIFrame() {
-    $(".video").css("visibility", "visible");
     $(".videoIframe").css("width", "640");
     $(".videoIframe").css("height", "360");
+    $(".video").css("visibility", "visible");
+    $(".videoIframe").attr("src", videoUrl);
+    playVideo = true;
   }
   
   function closeIFrame() {
     $(".video").css("visibility", "hidden");
+    playVideo = false;
   }
   
