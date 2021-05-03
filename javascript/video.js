5bc2f486a7124d8e366ddf0cae6eb7075ce0ed5e
dajin.gao@gmail.com <dajin.gao@gmail.com>
2021-05-03 13:08:11 +0900

    version 0.3

--- javascript/video.js
+++ javascript/video.js
@@ -1,8 +1,9 @@
-var playVideo = false;
+var videoDisplay = false;
+var videoSound = false;
 var videoUrl = "https://www.youtube.com/embed/_Xbubk0c9uQ?autoplay=1&playsinline=1";
 
-function changeVideoDisplay() {
-  if(playVideo) {
+function changeDisplay() {
+  if(videoDisplay) {
     closeIFrame();
   } else {
     displayIFrame();
@@ -12,13 +13,18 @@ function changeVideoDisplay() {
 function displayIFrame() {
     $(".videoIframe").css("width", "640");
     $(".videoIframe").css("height", "360");
-    $(".video").css("visibility", "visible");
-    $(".videoIframe").attr("src", videoUrl);
-    playVideo = true;
+    $(".videoIframe").css("visibility", "visible");
+    if(!videoSound) {
+      $(".videoIframe").attr("src", videoUrl);
+    }
+    $(".change_display_button").text("動画非表示");
+    videoDisplay = true;
+    videoSound = true;
   }
   
   function closeIFrame() {
-    $(".video").css("visibility", "hidden");
-    playVideo = false;
+    $(".videoIframe").css("visibility", "hidden");
+    $(".change_display_button").text("動画表示");
+    videoDisplay = false;
   }
   
