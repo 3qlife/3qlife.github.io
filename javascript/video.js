40891f0d6d425184ee5b78ffea076f6a532b1152
dajin.gao@gmail.com <dajin.gao@gmail.com>
2021-05-04 13:07:31 +0900

    2.0

--- javascript/video.js
+++ javascript/video.js
@@ -17,7 +17,6 @@ function displayIFrame() {
     if (!videoSound) {
       $(".videoIframe").attr("src", videoUrl);
     }
-    resizeContentIframe(-$(".videoIframe").height());
     videoDisplay = true;
     videoSound = true;
   }
@@ -26,46 +25,21 @@ function displayIFrame() {
 function closeIFrame() {
   if(videoDisplay) {
     $(".videoIframe").css("display", "none");
-    resizeContentIframe($(".videoIframe").height());
     videoDisplay = false;
   }
 }
 
 function clickLeftNavi(videoId,contentId) {
-  
   videoUrl = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&playsinline=1';
   $(".videoIframe").attr("src", "");
   closeIFrame();
-  $(".content_iframe").attr("src", "contents/" + contentId + ".html");
+  $.ajaxSetup({cache:false}); 
+  $(".contentDiv").load("contents/" + contentId + ".html"); 
   videoDisplay = false;
   videoSound = false;
 }
 
-$(function () {
- $(".main").height(window.innerHeight-$(".main").offset().top-$(".footer").height()-28);
- $(".content_iframe").height($(".main").height()-25);
- if($(".content_iframe").height()<200) {
-   $(".main").height($(".videoIframe").height() + 200);
-   $(".content_iframe").height(200);
- }
-  $(".aside-1 a:first-child").click();
-});
-
-$(window).resize(function() {
-  $(".main").height(window.innerHeight-$(".main").offset().top-$(".footer").height());
-  $(".content_iframe").height($(".main").height()-25);
-  if($(".content_iframe").height()<200) {
-    $(".main").height($(".videoIframe").height() + 200);
-    $(".content_iframe").height(200);
-  }
-  if (videoDisplay) {
-    resizeContentIframe(-$(".videoIframe").height());
-    resizeVideoIframe()
-  }
-});
-
 function resizeVideoIframe(){
-
   if(window.innerWidth<640) {
     $(".videoIframe").css("width", window.innerWidth);
     $(".videoIframe").css("height", window.innerWidth*3/4);
@@ -75,10 +49,13 @@ function resizeVideoIframe(){
   }
 }
 
-function resizeContentIframe(px){
-  $(".content_iframe").height($(".content_iframe").height()+px);
-  if($(".content_iframe").height()<200) {
-    $(".main").height($(".videoIframe").height() + 200);
-    $(".content_iframe").height(200);
+$(function () {
+  $(".aside-1 a:first-child").click();
+});
+
+$(window).resize(function() {
+  if (videoDisplay) {
+    resizeVideoIframe()
   }
-}
+});
+
